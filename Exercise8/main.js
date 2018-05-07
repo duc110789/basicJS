f162184e202eca44c0eb86c574528130286b7cec
duclv <duc110789@gmail.com>
2018-05-07 15:59:58 +0700

    Guess a number

---
+++ Exercise8/main.js
@@ -0,0 +1,12 @@
+// for (var i = 1; i <= 10; i++) {
+    
+// }
+var number = Math.floor(Math.random() * 10) + 1;
+var user = prompt('user enter is number:  ');
+if (user === number) {
+    console.log('Gook work');
+}
+else {
+    console.log('Not matched')
+}
+console.log(number);
