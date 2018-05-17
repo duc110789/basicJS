986e68da329be6cbf046ec27733716228a9d8898
duclv <duc110789@gmail.com>
2018-05-17 16:38:41 +0700

    to difference between a given number

---
+++ Exercise15/main.js
@@ -0,0 +1,11 @@
+// To difference between a given number 13, if the number is greater 13 return double the absolute difference
+function difference(n) {
+    if (n < 13) {
+        return numb = 13 - n;
+    }
+    else {
+        return numb = (n - 13) * 2;
+    }
+}
+console.log(difference(2));
+console.log(difference(22));
