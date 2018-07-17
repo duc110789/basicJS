1d5cf9a6e50f5444703ab0ec9881581206698bcf
duc110789 <duc110789@gmail.com>
2018-07-17 14:41:37 +0700

    add file exercise22

---
+++ Exercise22/es6.js
@@ -0,0 +1,10 @@
+function add_string(x) {
+    if (x === 'Py') {
+        return x;
+    }
+    else {
+        return x + 'Py';
+    }
+}
+console.log(add_string('abc'));
+console.log(add_string('PY'));
