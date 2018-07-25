520e3e23f871b2cb4633b149a734cdca0664209c
duc110789 <duc110789@gmail.com>
2018-07-25 13:59:26 +0700

    Write a JS program to remove a character at the specified of given string and return the new string

---
+++ Exercise23/main.js
@@ -0,0 +1,12 @@
+// remove_char(str, index)
+function remove_char(str, char_pos) {
+    part1 = str.substring(0, char_pos); // char_pos = 2; part1(0,2) -> Pyt
+
+    part2 = str.substring(char_pos + 2, str.length); // char_pos = 3; part1(3,5) -> hon
+    return (part1 + part2);
+}
+
+console.log(remove_char('Python', 0));
+console.log(remove_char('Python', 1));
+console.log(remove_char('Python', 2));
+console.log(remove_char('Python', 3));
