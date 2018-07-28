c703cb2b345c4688734af0a1d135c6e8a934f56c
MacBook <macbook@MacBooks-MBP.net.fpt>
2018-07-28 18:19:42 +0700

    Write a JS program to check if two numbers in range

---
+++ Exercise33/es6.js
@@ -0,0 +1,15 @@
+function numb_check(a) {
+    if (Number.isInteger(a)) {
+        if ((a >= 40 && a <= 60) || (a >= 70 && a <= 100)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+console.log(numb_check(100));
+console.log(numb_check('abc'));
